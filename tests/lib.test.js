fa8d2866e208832e738658342da04b7204992638
wgeorge1990 <williameverettgeorge@icloud.com>
2019-11-05 16:31:53 -0500

    Refactored first three tests into a suite using describe and (it).insteadOf(test)

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,17 +1,20 @@
 const lib = require('../lib');
 //Calculate execution paths and make sure test covers all paths.
 
-test('absolute - should return a positive number if input is positive.', () => {
+describe('absolute', () => {
+    it('It should return a positive number if input is positive.', () => {
     const result = lib.absolute(1);
     expect(result).toBe(1);
-});
+    });
 
-test('absolute - should return a positive number if input is negative.', () => {
-    const result = lib.absolute(-1);
-    expect(result).toBe(1);
-});
+    it('It should return a positive number if input is negative.', () => {
+        const result = lib.absolute(-1);
+        expect(result).toBe(1);
+    });
 
-test('absolute - should return 0  if input is 0.', () => {
-    const result = lib.absolute(0);
-    expect(result).toBe(0);
-});
+    it('It should return 0  if input is 0.', () => {
+        const result = lib.absolute(0);
+        expect(result).toBe(0);
+    });
+})
+
